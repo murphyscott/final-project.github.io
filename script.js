6a03df286bfe1a8b04bcd55695c641f5efe8cdeb
murphyscott <70599122+murphyscott@users.noreply.github.com>
2020-12-09 18:37:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ document.getElementById("foot01").innerHTML =
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
-"<li><a href='customers.html'>Data</a></li>" +
-"<li><a href='about.html'>About</a></li>" +
+"<li><a href='customers.html'>Republican Candidate</a></li>" +
+"<li><a href='about.html'>Basics</a></li>" +
+"<li><a href='about.html'>Democratic Candidate</a></li>" +
+"<li><a href='about.html'>Helpful Sources</a></li>"
 "</ul>";
